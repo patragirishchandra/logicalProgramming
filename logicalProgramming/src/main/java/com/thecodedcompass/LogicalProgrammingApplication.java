8976ecf3d434cf92d3aeca5ad694aff0d1b2d21b
Girish cahndra Patra <patragirishchandra@gmail.com>
2026-04-08 12:33:15 +0530

    added app skeleton

---
+++ logicalProgramming/src/main/java/com/thecodedcompass/LogicalProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.thecodedcompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicalProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicalProgrammingApplication.class, args);
+	}
+
+}
---
+++ logicalProgramming/src/test/java/com/thecodedcompass/LogicalProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecodedcompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicalProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
